53f320c85c80587ac85b10075b20185558120ae0
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2023-05-04 16:48:37 -0300

    Fixing server

--- ArraySorter/index.js
+++ ArraySorter/index.js
@@ -5,40 +5,30 @@ const { bubbleSort, quickSort } = require('./sorting')
 
 app.use(express.json())
 
-let stressCount = 0
+let stressCount = 1
 
 app.post('/', function (req, res) {
-    function handleArraySorting() {
-        // Prepares error response in case of empty array
-        let status = 400
-        let result = {
-            "msg": "No array was sent.\n",
-            "sorted_array": []
-        }
-        let unsortedArray = req.body['array']
+    // Prepares error response in case of empty array
+    let status = 400
+    let result = {
+        "msg": "No array was sent.\n",
+        "sorted_array": []
+    }
+    let unsortedArray = req.body['array']
 
-        // Sorts array
-        if (unsortedArray) {
-            console.log(`Starting sort ${stressCount}`)
-            unsortedArray = unsortedArray.map((v) => parseInt(v))
-            result['msg'] = "Here is your sorted array.\n"
-            result['sorted_array'] = quickSort(unsortedArray)
-            console.log(`Sorting ${stressCount++} finished`)
+    // Sorts array
+    if (unsortedArray) {
+        console.log(`Starting sort ${stressCount}`)
+        unsortedArray = unsortedArray.map((v) => parseInt(v))
+        result['msg'] = "Here is your sorted array.\n"
+        result['sorted_array'] = quickSort(unsortedArray)
+        console.log(`Sorting ${stressCount++} finished`)
 
-            status = 200
-        }
-
-        // Sends response
-        res.status(status).send(result)
+        status = 200
     }
 
-    // Se par espera 2 segundos antes de responder
-    if (stressCount % 2 == 0) {
-        setTimeout(() => {
-            handleArraySorting();
-        }, 2000);
-    } else handleArraySorting();
-
+    // Sends response
+    res.status(status).send(result)
 })
 
 app.listen(port, () => {
