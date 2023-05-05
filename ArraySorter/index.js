04d6c5a84b71bf1b5e03efcfe79637a23e2e7bc2
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2023-05-05 17:08:46 -0300

    Update

--- ArraySorter/index.js
+++ ArraySorter/index.js
@@ -7,8 +7,15 @@ app.use(express.json())
 
 let stressCount = 1
 
-app.post('/', function (req, res) {
-    // Prepares error response in case of empty array
+app.post('/bubblesort', function (req, res) {
+    sortRequestHandler(req, res, bubbleSort)
+})
+
+app.post('/quicksort', function (req, res) {
+    sortRequestHandler(req, res, quickSort)
+})
+
+function sortRequestHandler(req, res, algorithm) {
     let status = 400
     let result = {
         "msg": "No array was sent.\n",
@@ -16,12 +23,12 @@ app.post('/', function (req, res) {
     }
     let unsortedArray = req.body['array']
 
-    // Sorts array
+    // Sorts array and sets response and status
     if (unsortedArray) {
         console.log(`Starting sort ${stressCount}`)
         unsortedArray = unsortedArray.map((v) => parseInt(v))
         result['msg'] = "Here is your sorted array.\n"
-        result['sorted_array'] = quickSort(unsortedArray)
+        result['sorted_array'] = algorithm(unsortedArray)
         console.log(`Sorting ${stressCount++} finished`)
 
         status = 200
@@ -29,7 +36,7 @@ app.post('/', function (req, res) {
 
     // Sends response
     res.status(status).send(result)
-})
+}
 
 app.listen(port, () => {
     console.log(`I'm working on http://localhost:${port}:)`)
